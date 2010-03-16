d2402af73b4c0cecd748e32d8136817d77e05644
oisin <oisin@ethereal>
2010-03-16 15:02:04 +0000

    renaming to web and putting inside the evasion namespace.

--- evasion/web/config/environment.py
+++ evasion/web/config/environment.py
@@ -100,7 +100,7 @@ def load_environment(global_conf, app_conf, websetup=False):
     # If nothing is found in the configuration then the default 
     # 'evasion.web' interface will be used.
     #
-    loaded_modules = app_conf.get('webadmin_modules', 'evasion.web')
+    loaded_modules = app_conf.get('web_modules', 'evasion.web')
     loaded_modules = [m for m in loaded_modules.split(',') if m]
     get_log().info("load_environment: director evasion.web modules '%s'." % loaded_modules)
     
